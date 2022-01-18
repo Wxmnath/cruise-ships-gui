b15ecc544517ea127779250449451681737fba1f
Wxmnath <Wxmnath@aol.com>
2022-01-18 20:16:13 +0000

    Make Port test DRY

--- _tests_/Port.test.js
+++ _tests_/Port.test.js
@@ -1,40 +1,39 @@
 const Port = require("../src/Port.js");
 
 describe("Port", () => {
-  it("can be instantiated", () => {
-    expect(new Port()).toBeInstanceOf(Object);
-  });
+  describe("with ships and an itinerary", () => {
+    let ship;
+    let port;
+    let titanic;
+    let queenMary;
 
-  it("Port", () => {
-    const port = new Port("Dover");
-    expect(port.portName).toBe("Dover");
-  });
+    beforeEach(() => {
+      port = new Port("Dover");
+      ship = {};
+      titanic = {};
+      queenMary = {};
+    });
 
-  it("can add a ship", () => {
-    const port = new Port("Dover");
-    const ship = {};
+    it("can be instantiated", () => {
+      expect(new Port()).toBeInstanceOf(Object);
+    });
 
-    port.addShip(ship);
-    /* You'll notice here that we've not used the Ship constructor for 
-    creating a Ship instance, but rather an object literal. The reason is 
-    because we've already tested Ship in the ship test suite. Here we're 
-    just checking that a port can store a collection of entities - 
-    at the moment we don't even need to be concerned with the interface 
-    of the object we add to a port. When we do need to be concerned with 
-    the interface then we can use mocks. */
+    it("Port", () => {
+      expect(port.portName).toBe("Dover");
+    });
 
-    expect(port.ships).toContain(ship);
-  });
+    it("can add a ship", () => {
+      port.addShip(ship);
 
-  it("can remove a ship", () => {
-    const port = new Port("Dover");
-    const titanic = {};
-    const queenMary = {};
+      expect(port.ships).toContain(ship);
+    });
 
-    port.addShip(titanic);
-    port.addShip(queenMary);
-    port.removeShip(queenMary);
+    it("can remove a ship", () => {
+      port.addShip(titanic);
+      port.addShip(queenMary);
+      port.removeShip(queenMary);
 
-    expect(port.ships).toEqual([titanic]);
+      expect(port.ships).toEqual([titanic]);
+    });
   });
 });
